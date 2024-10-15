af5e2341c4f18f112f65058e38fc4428d452bec9
Vladyslav Korniienko <maradonna624@gmail.com>
2024-10-15 16:19:17 +0300

    Add entities

---
+++ ChatApp.Domain/Entities/ChatRoom.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatApp.Domain.Entities
+{
+    public class ChatRoom
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public List<User> Users { get; set; }
+    }
+}
---
+++ ChatApp.Domain/Entities/Message.cs
@@ -0,0 +1,13 @@
+﻿namespace ChatApp.Domain.Entities
+{
+    public class Message
+    {
+        public string Id { get; set; }
+        public string Content { get; set; }
+        public DateTime TimeSent { get; set; }
+        public string UserId {  get; set; }
+        public User User { get; set; }
+        public string ChatId { get; set; }
+        public ChatRoom Chat { get; set; }
+    }
+}
