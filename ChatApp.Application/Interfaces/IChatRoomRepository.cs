8b0e39d69df303d8bebc4b343fae7aa444669413
Vladyslav Korniienko <maradonna624@gmail.com>
2024-11-02 21:10:28 +0200

    Restructure Application layer and add Interfaces path

---
+++ ChatApp.Application/Interfaces/IChatRoomRepository.cs
@@ -0,0 +1,17 @@
+﻿using ChatApp.Domain.Entities;
+
+
+namespace ChatApp.Application.Repositories
+{
+    public interface IChatRoomRepository
+    {
+        Task<ChatRoom> GetByIdAsync(string chatRoomId);
+        Task<IEnumerable<ChatRoom>> GetAllAsync();
+        Task AddAsync(ChatRoom chatRoom);
+        Task UpdateAsync(ChatRoom chatRoom);
+        Task DeleteAsync(string id);
+        Task<IEnumerable<Message>> GetRoomAsync(string chatRoomId);
+        Task AddUserToRoomAsync(ChatRoom chatRoom, User user);
+        Task RemoveUserFromRoomAsync(ChatRoom chatRoom, User user);
+    }
+}
---
+++ ChatApp.Application/Interfaces/IChatRoomService.cs
@@ -0,0 +1,18 @@
+﻿using ChatApp.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatApp.Application.Interfaces
+{
+    public interface IChatRoomService
+    {
+        Task<ChatRoom> CreateRoomAsync(string roomName, User creator);
+        Task AddUserToRoomAsync(string roomId, User user);
+        Task RemoveUserFromRoomAsync(string roomId, User user);
+        Task<IEnumerable<ChatRoom>> GetAllRoomsAsync();
+        Task<ChatRoom> GetRoomAsync(string roomId);
+    }
+}
