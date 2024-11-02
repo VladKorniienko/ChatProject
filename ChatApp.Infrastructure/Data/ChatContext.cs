443b05543e5305a148760c802a6501821a00ef71
Vladyslav Korniienko <maradonna624@gmail.com>
2024-11-02 21:08:30 +0200

    Add DbContext and configuration

---
+++ ChatApp.Infrastructure/Data/ChatContext.cs
@@ -0,0 +1,27 @@
+﻿using ChatApp.Domain.Entities;
+using ChatApp.Infrastructure.Data.Configurations;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatApp.Infrastructure.Data
+{
+    public class ChatContext : IdentityDbContext<User>
+    {
+        public ChatContext(DbContextOptions<ChatContext> options) : base(options)
+        {
+        }
+        public DbSet<ChatRoom> ChatRooms { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfiguration(new UserConfiguration());
+
+        }
+    }
+}
---
+++ ChatApp.Infrastructure/Data/Configurations/UserConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using ChatApp.Domain.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatApp.Infrastructure.Data.Configurations
+{
+    public class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.Property(u => u.UserName)
+                .IsRequired();
+            builder.HasIndex(u => u.UserName)
+                .IsUnique();
+            builder.Property(u => u.Email)
+                .IsRequired();
+            builder.HasIndex(u => u.Email)
+                .IsUnique();
+        }
+    }
+}
