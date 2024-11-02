969ca3b8203c42e0ac43ba86d5d4385089287396
Vladyslav Korniienko <maradonna624@gmail.com>
2024-11-02 15:52:29 +0200

    Add UnitOfWork

---
+++ ChatApp.Application/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using ChatApp.Application.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatApp.Application.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        Task<int> SaveChangesAsync();
+        IChatRoomRepository ChatRoomRepository { get; }
+        //IMessageRepository MessageRepository { get; }
+        IUserRepository UserRepository { get; }
+    }
+}
---
+++ ChatApp.Infrastructure/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,40 @@
+﻿using ChatApp.Application.Interfaces;
+using ChatApp.Application.Repositories;
+using ChatApp.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatApp.Infrastructure.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        protected readonly ChatContext _dbContext;
+        public IChatRoomRepository ChatRoomRepository { get; }
+        //public IMessageRepository MessageRepository { get; }
+        public IUserRepository UserRepository { get; }
+
+        public UnitOfWork(ChatContext context,
+                          IChatRoomRepository chatRoomRepository,
+                          //IMessageRepository messageRepository,
+                          IUserRepository userRepository)
+        {
+            _dbContext = context;
+            ChatRoomRepository = chatRoomRepository;
+            //MessageRepository = messageRepository;
+            UserRepository = userRepository;
+        }
+        public async Task<int> SaveChangesAsync()
+        {
+            return await _dbContext.SaveChangesAsync();
+        }
+        public void Dispose()
+        {
+            _dbContext.Dispose();
+        }
+
+    }
+}
