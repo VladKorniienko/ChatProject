d601babcda9f1065cd83fea3489b57f50a243424
Vladyslav Korniienko <maradonna624@gmail.com>
2024-11-06 13:33:31 +0200

    Add extensions class for DB setup

---
+++ ChatApp.Infrastructure/ServiceExtensions.cs
@@ -0,0 +1,28 @@
+﻿using ChatApp.Application.Interfaces;
+using ChatApp.Application.Repositories;
+using ChatApp.Infrastructure.Data;
+using ChatApp.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatApp.Infrastructure
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<IChatRoomRepository, ChatRoomRepository>();
+            services.AddScoped<IUnitOfWork, UnitOfWork.UnitOfWork>();
+            var connectionString = configuration.GetConnectionString("SqlServer");
+            services.AddDbContext<ChatContext>(options =>
+                     options.UseSqlServer(connectionString));
+        }
+    }
+}
